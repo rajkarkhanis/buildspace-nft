c183a9e1c426cabe2ff86421bfdbdbd58bff605c
rajkarkhanis <rkarkhanis00@gmail.com>
2021-11-23 10:24:39 +0530

    ERC721 and minting two NFTs

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,10 +2,35 @@
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicNFT{
-    constructor(){
+// We define that our contract is ERC721 i.e. an NFT
+contract MyEpicNFT is ERC721URIStorage{
+    // Every NFT in the collection has a unique number
+    // We can assign ID to each NFT with Counters
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    // When creating NFT we need to pass two things
+    // A name (SquareNFT) and a token symbol (SQUARE)
+    constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("Waddup");
+    }
+
+    // This is the mint function
+    // It will be called by users when they hit "Mint NFT" on the website
+    function makeAnEpicNFT() public {
+        // Get current tokenId, starts at 0
+        uint256 newTokenId = _tokenIds.current();
+        // Mint the NFT and send to the user with msg.sender
+        _safeMint(msg.sender, newTokenId);
+        // set the data of the NFT, like an image or a video
+        _setTokenURI(newTokenId, "https://jsonkeeper.com/b/C4HX");
+        console.log("A new NFT w/ID %s has been minted to %s", newTokenId, msg.sender);
+        // increment counter for the next NFT
+        _tokenIds.increment();
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -3,6 +3,14 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("NFT contract deployed to: ", nftContract.address);
+
+    // Calling the minting function
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+
+    // Calling it for a second time
+    txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
 };
 
 const runMain = async () => {
